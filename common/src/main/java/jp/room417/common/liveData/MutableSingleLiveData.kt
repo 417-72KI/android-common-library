dbfbd43ac074fe938acfa6283ab962bf5e08707b
417-72KI <417.72ki@gmail.com>
2021-04-11 04:52:03 +0900

    fix

--- common/src/main/java/jp/room417/common/liveData/MutableSingleLiveData.kt
+++ common/src/main/java/jp/room417/common/liveData/MutableSingleLiveData.kt
@@ -1,8 +1,7 @@
 package jp.room417.common.liveData
 
-class MutableSingleLiveData<T> : SingleLiveData<T>() {
-    override fun getValue(): T? = source.value
-    public override fun setValue(value: T?) = value.let { source.value = it }
+class MutableSingleLiveData<T>(initialValue: T? = null) : SingleLiveData<T>(initialValue) {
+    public override fun setValue(value: T?) = super.setValue(value)
 
     @get:JvmName("_getValue")
     @set:JvmName("_setValue")
--- common/src/main/java/jp/room417/common/liveData/SingleLiveData.kt
+++ common/src/main/java/jp/room417/common/liveData/SingleLiveData.kt
@@ -6,20 +6,44 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import java.util.concurrent.atomic.AtomicBoolean
 
-open class SingleLiveData<T> : LiveData<T>() {
-    protected val source = MutableLiveData<T>()
+open class SingleLiveData<T>(initialValue: T? = null) : LiveData<T>() {
+    private val source = MutableLiveData<T>().apply {
+        setValue(initialValue)
+    }
     private val firstIgnore = AtomicBoolean(true)
 
-    fun observe(owner: LifecycleOwner, observer: ((T) -> Unit)) {
-        source.observe(owner, Observer {
-            if (firstIgnore.getAndSet(false)) {
-                return@Observer
-            }
-            it?.let { observer(it) }
-        })
+    @Suppress("MemberVisibilityCanBePrivate")
+    fun observe(owner: LifecycleOwner, observer: ((T) -> Unit)) = source.observe(owner, Observer {
+        if (firstIgnore.getAndSet(false)) {
+            return@Observer
+        }
+        it?.let { observer(it) }
+    })
+
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) =
+        observe(owner) { observer.onChanged(it) }
+
+    @Suppress("MemberVisibilityCanBePrivate")
+    fun observeForever(observer: ((T) -> Unit)) = source.observeForever(Observer {
+        if (firstIgnore.getAndSet(false)) {
+            return@Observer
+        }
+        it?.let { observer(it) }
+    })
+
+    override fun observeForever(observer: Observer<in T>) =
+        observeForever { observer.onChanged(it) }
+
+    override fun getValue(): T? = source.value
+
+    override fun setValue(value: T?) {
+        source.value = value
     }
 
-    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
-        observe(owner) { observer.onChanged(it) }
-    }
+    override fun postValue(value: T) = source.postValue(value)
+
+    override fun hasObservers(): Boolean = source.hasObservers()
+    override fun hasActiveObservers(): Boolean = source.hasActiveObservers()
+    override fun hashCode(): Int = source.hashCode()
+    override fun equals(other: Any?): Boolean = source == other
 }
