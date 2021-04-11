7a57007dc4c92718524cbee4f063b65459bee1b0
417-72KI <417.72ki@gmail.com>
2021-04-12 00:51:50 +0900

    add mock Logger https://dev.classmethod.jp/articles/android_local_test_mock/

---
+++ common/src/test/java/android/util/Log.java
@@ -0,0 +1,48 @@
+package android.util;
+
+@SuppressWarnings({"unused", "RedundantSuppression"})
+public class Log {
+    public static int v(String tag, String msg) {
+        return 0;
+    }
+
+    public static int v(String tag, String msg, Throwable tr) {
+        return 0;
+    }
+
+    public static int d(String tag, String msg) {
+        return 0;
+    }
+
+    public static int d(String tag, String msg, Throwable tr) {
+        return 0;
+    }
+
+    public static int i(String tag, String msg) {
+        return 0;
+    }
+
+    public static int i(String tag, String msg, Throwable tr) {
+        return 0;
+    }
+
+    public static int w(String tag, String msg) {
+        return 0;
+    }
+
+    public static int w(String tag, String msg, Throwable tr) {
+        return 0;
+    }
+
+    public static int w(String tag, Throwable tr) {
+        return 0;
+    }
+
+    public static int e(String tag, String msg) {
+        return 0;
+    }
+
+    public static int e(String tag, String msg, Throwable tr) {
+        return 0;
+    }
+}
