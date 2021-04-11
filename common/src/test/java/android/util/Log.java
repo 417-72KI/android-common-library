0b6a1f6e62368c07fd4379a533fc71f01670aecb
417-72KI <417.72ki@gmail.com>
2021-04-12 00:55:28 +0900

    add javadoc

--- common/src/test/java/android/util/Log.java
+++ common/src/test/java/android/util/Log.java
@@ -1,5 +1,9 @@
 package android.util;
 
+/**
+ * Test stub for `android.util.Log`
+ * @see <a href="https://dev.classmethod.jp/articles/android_local_test_mock/">here</a>
+ */
 @SuppressWarnings({"unused", "RedundantSuppression"})
 public class Log {
     public static int v(String tag, String msg) {
