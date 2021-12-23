0e49646bbfa40f44e3db30c3e5a04a0c131531ca
417-72KI <417.72ki@gmail.com>
2021-12-24 02:11:20 +0900

    fix spacing

--- twitter/src/main/java/jp/room417/twitter/service/DefaultTwitterServiceFactory.kt
+++ twitter/src/main/java/jp/room417/twitter/service/DefaultTwitterServiceFactory.kt
@@ -2,7 +2,7 @@ package jp.room417.twitter.service
 
 import android.content.Context
 
-class DefaultTwitterServiceFactory: TwitterServiceFactory() {
+class DefaultTwitterServiceFactory : TwitterServiceFactory() {
     override fun createService(context: Context, apiKey: String, secret: String): TwitterService =
         TwitterServiceImpl(context, apiKey, secret)
 }
