af29f454280b443f60f14a5399b57ae5bfa9ad25
417-72KI <417.72ki@gmail.com>
2022-12-29 03:00:33 +0900

    add twitter auth

--- app-sample/src/main/java/jp/room417/common/sampleApp/scene/MainActivity.kt
+++ app-sample/src/main/java/jp/room417/common/sampleApp/scene/MainActivity.kt
@@ -4,18 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement.spacedBy
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import jp.room417.common.component.base.debugLog
+import jp.room417.common.sampleApp.BuildConfig
+import jp.room417.common.sampleApp.scene.auth.TwitterOAuthActivity
 import jp.room417.common.sampleApp.ui.theme.AndroidcommonlibraryTheme
+import jp.room417.twitter.component.OAuthActivityBuilder
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -25,15 +33,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             val viewModel: MainViewModel = hiltViewModel()
             debugLog("${viewModel.prefSys.getPrefBoolean("foo")}")
+            debugLog("hasAccessToken: ${viewModel.hasAccessToken}")
 
             AndroidcommonlibraryTheme {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colors.background),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Greeting("World")
+                MainScreen(viewModel.hasAccessToken) {
+                    OAuthActivityBuilder(
+                        TwitterOAuthActivity::class.java,
+                        this,
+                        BuildConfig.TWITTER_CALLBACK_URL,
+                        BuildConfig.TWITTER_API_KEY,
+                        BuildConfig.TWITTER_API_SECRET
+                    ).buildIntent().let { startActivity(it) }
                 }
             }
         }
@@ -41,14 +51,39 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
+private fun MainScreen(hasAccessToken: Boolean, onAuthAction: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colors.background),
+        contentAlignment = Center
+    ) {
+        Column(
+            horizontalAlignment = CenterHorizontally,
+            verticalArrangement = spacedBy(8.dp)
+        ) {
+            if (hasAccessToken) {
+                Greeting("Twitter")
+            } else {
+                Greeting("World")
+                Button(onClick = onAuthAction) {
+                    Text("Auth twitter")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+private fun DefaultPreview() {
     AndroidcommonlibraryTheme {
-        Greeting("Android")
+        MainScreen(hasAccessToken = true) {
+        }
     }
 }
--- app-sample/src/main/java/jp/room417/common/sampleApp/scene/MainViewModel.kt
+++ app-sample/src/main/java/jp/room417/common/sampleApp/scene/MainViewModel.kt
@@ -4,11 +4,19 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jp.room417.common.util.PrefSys
+import jp.room417.twitter.service.TwitterService
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    application: Application
+    application: Application,
+    private val twitterService: TwitterService
 ) : AndroidViewModel(application) {
     val prefSys = PrefSys(application)
+
+    val hasAccessToken
+        get() = twitterService.hasAccessToken
+
+    val twitter
+        get() = twitterService.twitter
 }
