458cfbebbffcfd2732ff48d7362a6a9555d1a96c
417-72KI <417.72ki@gmail.com>
2023-01-01 21:58:40 +0900

    set Twitter object as `deprecated`

--- twitter/src/main/java/jp/room417/twitter/service/TwitterService.kt
+++ twitter/src/main/java/jp/room417/twitter/service/TwitterService.kt
@@ -4,6 +4,10 @@ import twitter4j.Twitter
 import twitter4j.auth.AccessToken
 
 interface TwitterService {
+    @Deprecated(
+        message = "Will be no longer accessible.",
+        level = DeprecationLevel.WARNING
+    )
     val twitter: Twitter
     val hasAccessToken: Boolean
 
