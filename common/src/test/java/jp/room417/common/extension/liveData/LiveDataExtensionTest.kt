170616d699ee24aeda240332fc73641765a87d1a
417-72KI <417.72ki@gmail.com>
2026-04-03 01:06:10 +0900

    fix

--- common/src/test/java/jp/room417/common/extension/liveData/LiveDataExtensionTest.kt
+++ common/src/test/java/jp/room417/common/extension/liveData/LiveDataExtensionTest.kt
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.ArgumentsProvider
 import org.junit.jupiter.params.provider.ArgumentsSource
+import org.junit.jupiter.params.support.ParameterDeclarations
 import java.util.stream.Stream
 
 class LiveDataExtensionTest {
@@ -19,7 +20,7 @@ class LiveDataExtensionTest {
     }
 
     private class IsTrueTestCase : ArgumentsProvider {
-        override fun provideArguments(context: ExtensionContext?): Stream<out Arguments> = Stream.of(
+        override fun provideArguments(parameters: ParameterDeclarations, context: ExtensionContext): Stream<out Arguments> = Stream.of(
             Arguments.arguments(true, true),
             Arguments.arguments(null, false),
         )
